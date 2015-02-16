2cdfd0a8230b44b7573fab42e86c0af9b7f2fdcb
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-02-16 15:24:32 -0500

    Update New-BalloonNotification

--- WinFormPS.psm1
+++ WinFormPS.psm1
@@ -687,11 +687,44 @@ function Get-ListViewItem
 
 function New-BalloonNotification
 {
+<#
+	.SYNOPSIS
+		The New-BalloonNotification function will show a message to the user in the notification area of Windows.
+	
+	.DESCRIPTION
+		The New-BalloonNotification function will show a message to the user in the notification area of Windows.
+	
+	.PARAMETER BalloonIcon
+		Specifies the Icon to show. Default is None
+	
+	.PARAMETER BalloonTipText
+		Specifies the Message to show.
+	
+	.PARAMETER BalloonTipTitle
+		Specifies the Title to show.
+	
+	.PARAMETER CustomIconPath
+		Specifies the custom icon literal path to use. Default will use the PowerShell icon.
+	
+	.PARAMETER TimeOut
+		Specifies the display duration of the message. Default is 10000 milliseconds
+	
+	.EXAMPLE
+		PS C:\> New-BalloonNotification -BalloonTipText "test" -BalloonTipTitle "Title" -BalloonIcon Error
+	
+	.NOTES
+		Francois-Xavier Cat
+		@lazywinadm
+		www.lazywinadmin.com
+#>
 	[CmdletBinding()]
 	PARAM (
 		[String]$CustomIconPath = "C:\Windows\WinSxS\amd64_microsoft-windows-dxp-deviceexperience_31bf3856ad364e35_10.0.9926.0_none_220133b3b110f55a\sync.ico",
+	
 		[int]$TimeOut = "10000",
-		[System.Windows.Forms.ToolTipIcon]$BalloonIcon,
+	
+		[ValidateSet('None', 'Info', 'Warning', 'Error')]
+		$BalloonIcon = "None",
 		$BalloonTipText,
 		$BalloonTipTitle
 	)
@@ -702,10 +735,19 @@ function New-BalloonNotification
 	}
 	PROCESS
 	{
+		
+		# Create Balloon Notification object
 		$BalloonNotification = New-Object -TypeName System.Windows.Forms.NotifyIcon
 		
 		IF ($PSBoundParameters["CustomIconPath"]) { $BalloonNotification.Icon = $CustomIconPath }
+		ELSE
+		{
+			# Get the Icon of PowerShell
+			$path = Get-Process -id $pid | Select-Object -ExpandProperty Path
+			$BalloonNotification.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+		}
 		
+		# Set properties of the object
 		$BalloonNotification.BalloonTipIcon = $BalloonIcon
 		$BalloonNotification.BalloonTipText = $BalloonTipText
 		$BalloonNotification.BalloonTipTitle = $BalloonTipTitle
@@ -714,7 +756,10 @@ function New-BalloonNotification
 		$BalloonNotification.ShowBalloonTip($TimeOut)
 	}
 	END
-	{ }
+	{
+		# Get rid of the Balloon
+		#$BalloonNotification.Dispose()
+	}
 }#New-BalloonNotification
 
 function New-OpenFileDialog
