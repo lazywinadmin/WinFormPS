238b9fa05137bd657f401196cae88eef21fb5473
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2016-02-25 12:16:47 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -13,19 +13,19 @@ if ('' -eq $InstallDirectory)
         Write-Warning "$personalModules is not in `$env:PSModulePath"
     }
 
-    if (!(Test-Path $personalModules)) {
+    if (-not(Test-Path $personalModules)) {
         Write-Error "$personalModules does not exist"
     }
 
     $InstallDirectory = Join-Path -Path $personalModules -ChildPath WinFormPS
 }
 
-if (!(Test-Path $InstallDirectory)) {
+if (-not(Test-Path $InstallDirectory)) {
     $null = mkdir $InstallDirectory
 }
 
-$wc = New-Object System.Net.WebClient
+$WebClient = New-Object -typename System.Net.WebClient
 $fileList |
     ForEach-Object {
-        $wc.DownloadFile("https://raw.github.com/LazyWinAdmin/WinFormPS/master/$_","$installDirectory\$_")
+        $WebClient.DownloadFile("https://raw.github.com/LazyWinAdmin/WinFormPS/master/$_","$installDirectory\$_")
     }
